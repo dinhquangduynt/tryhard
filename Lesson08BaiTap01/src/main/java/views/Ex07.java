a71ad06e4bcdd81c0ca78af92ae00584c0fb9dc2
Duy Dinh <dinhquangduynt@gmail.com>
2020-04-01 11:05:28 +0700

    duy submit code 1

--- Lesson08BaiTap01/src/main/java/views/Ex07.java
+++ Lesson08BaiTap01/src/main/java/views/Ex07.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ex07 {
 
 	public static void main(String[] args) {
-
+		//duy test git commit 
 		String inputString = input();
 		caculateTheLengthOfSubstring(inputString);
 	}
